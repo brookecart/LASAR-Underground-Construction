fbcc7a74e1ae3c7a5de2822a2a980519f895d740
not nick <nicolaskeil@hotmail.com>
2017-04-27 15:19:31 -0700

    jobs page

---
+++ jobs.php
@@ -0,0 +1,9 @@
+<?php
+            require('modules/navbar.php') //Head
+        ?>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>    
+</body>
+</html>
