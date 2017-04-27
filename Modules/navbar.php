46fa3b750fa588c349760cbbcc025faeb12d2c95
not nick <nicolaskeil@hotmail.com>
2017-04-27 14:43:22 -0700

    stuff

---
+++ Modules/navbar.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping the Jam!</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="shortcut icon" type="image/x-icon" href="tabimage.png" />
+    <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+  </head>
+      <body style="background-color: #B6A19E">
+       <div class="large-3 columns">
+        <a href="index.php">
+        <img src="images/logo.png">
+            </a>
+    </div>     
+    <div class="top-bar" style="background-color: #FBA100">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu style="background-color: #6BBAA7;">
+      <li>
+        <a href="accomplishments.php" style="color: white;">Accomplishments</a>
+        <ul class="menu vertical" data-click-open style="background-color: #6BBAA7;">
+          <li><a href="catspace.php" style="color: white;">AREA1</a></li>
+          <li><a href="catacc.php" style="color: white;">AREA2</a></li>
+          <li><a href="catcloth.php" style="color: white;">AREA3</a></li>
+        </ul>
+      </li>
+      <li><a href="about.php" style="color: white;">About</a></li>
+      <li><a href="contact.php" style="color: white;">Contact</a></li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="dropdown menu" data-dropdown-menu style="background-color: #6BBAA7;">
+      <li>
+        <a href="application.php" style="color: white;">Apply</a>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="top-bar">
+  <div class="top-bar-title">
+    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+      <button class="menu-icon dark" type="button" data-toggle></button>
+    </span>
+    <strong>Site Title</strong>
+  </div>
+  <div id="responsive-menu">
+    <div class="top-bar-left">
+      <ul class="dropdown menu" data-dropdown-menu>
+        <li>
+          <a href="#">One</a>
+          <ul class="menu vertical">
+            <li><a href="#">One</a></li>
+            <li><a href="#">Two</a></li>
+            <li><a href="#">Three</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Two</a></li>
+        <li><a href="#">Three</a></li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><input type="search" placeholder="Search"></li>
+        <li><button type="button" class="button">Search</button></li>
+      </ul>
+    </div>
+  </div>
+</div>
