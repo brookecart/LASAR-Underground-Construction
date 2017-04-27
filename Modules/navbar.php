7e76c080107e5df79b53f122b2a0d59b409e5139
not nick <nicolaskeil@hotmail.com>
2017-04-27 15:04:53 -0700

    better navber

--- Modules/navbar.php
+++ Modules/navbar.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping the Jam!</title>
+    <title>LASAR Underground</title>
     <link rel="stylesheet" href="css/foundation.css">
     <link rel="stylesheet" href="css/app.css">
-    <link rel="shortcut icon" type="image/x-icon" href="tabimage.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="cone.png" />
     <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
   </head>
       <body style="background-color: #B6A19E">
@@ -21,7 +21,7 @@
     <ul class="dropdown menu" data-dropdown-menu style="background-color: #6BBAA7;">
       <li>
         <a href="accomplishments.php" style="color: white;">Accomplishments</a>
-        <ul class="menu vertical" data-click-open style="background-color: #6BBAA7;">
+        <ul class="menu vertical" style="background-color: #6BBAA7;">
           <li><a href="catspace.php" style="color: white;">AREA1</a></li>
           <li><a href="catacc.php" style="color: white;">AREA2</a></li>
           <li><a href="catcloth.php" style="color: white;">AREA3</a></li>
@@ -37,35 +37,5 @@
         <a href="application.php" style="color: white;">Apply</a>
       </li>
     </ul>
-  </div>
-</div>
-<div class="top-bar">
-  <div class="top-bar-title">
-    <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-      <button class="menu-icon dark" type="button" data-toggle></button>
-    </span>
-    <strong>Site Title</strong>
-  </div>
-  <div id="responsive-menu">
-    <div class="top-bar-left">
-      <ul class="dropdown menu" data-dropdown-menu>
-        <li>
-          <a href="#">One</a>
-          <ul class="menu vertical">
-            <li><a href="#">One</a></li>
-            <li><a href="#">Two</a></li>
-            <li><a href="#">Three</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Two</a></li>
-        <li><a href="#">Three</a></li>
-      </ul>
-    </div>
-    <div class="top-bar-right">
-      <ul class="menu">
-        <li><input type="search" placeholder="Search"></li>
-        <li><button type="button" class="button">Search</button></li>
-      </ul>
-    </div>
   </div>
 </div>
