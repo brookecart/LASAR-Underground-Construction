50621676a86ec799363e03e4baa47ad74555cfec
not nick <nicolaskeil@hotmail.com>
2017-04-28 15:29:06 -0700

    something

---
+++ footer.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LASAR Underground</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="shortcut icon" type="image/x-icon" href="cone.png" />
+    <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+  </head>
+      <body class="lsr-body">
+       <div class="large-3 columns">
+        <a href="index.php">
+        <!--<img rc="images/logo.png">-->
+            </a>
+    </div>
+    
+    
+    
+</body>
+<footer>
+    <div id="footer" class="wrap row small-up-1 medium-up-3" style="padding: 50px;">
+        <div class="column">
+            <i class="fa fa-copyright" aria-hidden="true"> LASAR Underground Construction</i>
+            <p>Designed and developed by CART Group 6</p>
+        </div>
+        
+        <div class="column">
+            <p>We proudly service some of the leading companies in the Fresno Area.</p>
+            <ul>
+                <li>AT&amp;T Communications</li>
+                <li>Beal Properties</li>
+                <li>City of Clovis</li>
+                <li>City of Fresno</li>
+                <li>Fresno Flood Control District</li>
+                <li>Gerawan Farming</li>
+                <li>Moser Electric</li>
+            </ul>
+        </div>
+        
+        <div class="column">
+            <p>EMAIL undergroundemail@email.com</p>
+            <p>PHONE 559.291.0124</p>
+            <p>FAX 559.291.0624</p>
+        </div>
+        
+        <div class="column">
+            <p>LOCATIONS</p>
+            <p>2545 N. Sunnyside Ave.</p>
+        </div>
+        
+    </div>
+</footer>
+</html>
