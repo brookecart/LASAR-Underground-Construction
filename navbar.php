3fdde15129c0eab4313eeff183304bc5429665ee
not nick <nicolaskeil@hotmail.com>
2017-04-28 15:11:33 -0700

    navbar and somethign else

---
+++ navbar.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LASAR Underground</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="shortcut icon" type="image/x-icon" href="cone.png" />
+    <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+  </head>
+      <body class="lsr-body" style="background-color: #B6A19E">
+       <div class="large-3 columns">
+        <a href="index.php">
+        <!--<img rc="images/logo.png">-->
+            </a>
+    </div>     
+    
+ 
+ 
+ <div data-sticky-container class="lsr-nav" style="height: 72px;">
+  <div class="title-bar sticky is-anchored is-at-top" data-sticky data-options="marginTop:0;" style="width:100%" data-top-anchor="1">
+    <div class="title-bar-left">
+      <ul class="dropdown menu" data-dropdown-menu>
+      <li>
+        <a href="accomplishments.php" style="color: white;">Accomplishments</a>
+        <ul class="menu submenu is-dropdown-submenu first-sub vertical" style="background-color: black;" >
+          <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="catspace.php" style="color: white;">AREA1</a></li>
+          <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="catacc.php" style="color: white;">AREA2</a></li>
+          <li role="menuitem" class="is-submenu-item is-dropdown-submenu-item"><a href="catcloth.php" style="color: white;">AREA3</a></li>
+        </ul>
+      </li>
+      <li><a href="about.php" style="color: white;">About</a></li>
+      <li><a href="contact.php" style="color: white;">Contact</a></li>
+    </ul>
+      </div>
+    <div class="title-bar-right"><ul class="dropdown menu align-right" data-dropdown-menu>
+      <li>
+        <a href="application.php" style="color: white;">Apply</a>
+      </li>
+    </ul>
+    </div>
+  </div>
+</div>
+ 
+ 
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+   <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>   
+    </body>
+</html>
