796bafc02f777f4ea9a753eebc32f1238bcbbcff
blueyosh <lake.evan@student.CART.org>
2017-05-08 14:38:23 -0700

    asd

--- LASAR-Underground-Construction/Services.php
+++ LASAR-Underground-Construction/Services.php
@@ -2,15 +2,28 @@
 <br>
 <div class="lsrWrapper column"> 
     <div class="row">
+       <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
         <?php 
-            echo "<h1>Projects</h1>".
-            "<h2>Wiring</h2>".
-            "<h4>Wiring job 1</h4>";
+            echo "<h1>Services</h1>".
+            "<h4>Wiring</h4>";
             imgText("large-8","Images/IMG_1602.jpeg", "large-7",
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     "");
             echo "<br>".
-            "<h4>Wiring job 2</h4>";
+            "<h4>Solar</h4>";
             imgText("large-8","Images/IMG_1602.jpeg", "large-7",
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                     "");
