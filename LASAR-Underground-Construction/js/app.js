99e2f396831da90629e8d90a4db291369c1287b4
not nick <nicolaskeil@hotmail.com>
2017-05-08 15:31:08 -0700

    mmm

--- LASAR-Underground-Construction/js/app.js
+++ LASAR-Underground-Construction/js/app.js
@@ -2,5 +2,5 @@ $(document).foundation()
 window.open("www.youraddress.com","_self")
 
 $( document ).ready(function) {
-    $(".dropdown-button").dropdown(hover: false);
+    $(".dropdown-button").dropdown({ hover: false });
 }
