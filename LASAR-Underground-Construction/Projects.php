64cb5474e02afd7bbfd8bb4bc668fcace1bd7ca8
blueyosh <lake.evan@student.CART.org>
2017-05-08 13:10:25 -0700

    fixed my shite

--- LASAR-Underground-Construction/Projects.php
+++ LASAR-Underground-Construction/Projects.php
@@ -6,14 +6,14 @@
             echo "<h1>Projects</h1>".
             "<h2>Wiring</h2>".
             "<h4>Wiring job 1</h4>";
-            imgText("Images/IMG_1602.jpeg", "small-5",
+            imgText("large-8","Images/IMG_1602.jpeg", "large-7",
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    "small-5");
+                    "");
             echo "<br>".
             "<h4>Wiring job 2</h4>";
-            imgText("Images/IMG_1602.jpeg", "small-5",
+            imgText("large-8","Images/IMG_1602.jpeg", "large-7",
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    "small-5");
+                    "");
         ?>
     </div>
 </div>
