5568dbf280b127cac695e002486e8776e4ee25a3
not nick <nicolaskeil@hotmail.com>
2017-05-11 12:47:23 -0700

    edited dropdown javascript

--- LASAR-Underground-Construction/js/app.js
+++ LASAR-Underground-Construction/js/app.js
@@ -3,21 +3,16 @@ window.open("www.youraddress.com","_self")
 
 
 $(document).ready(function(){
-        $(".button-collapse").sideNav();
+         $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        });
         $('.parallax').parallax();
         $(".dropdown-button").dropdown({
             hover: false
         });
-        menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
     });
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+// end of jQuery name space
 
