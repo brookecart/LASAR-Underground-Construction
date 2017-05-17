4576c2ea9d69cf29240a3b343fb4de51edf4b11f
not nick <nicolaskeil@hotmail.com>
2017-05-17 12:53:28 -0700

    ok

--- LASAR-Underground-Construction/Services.php
+++ LASAR-Underground-Construction/Services.php
@@ -21,7 +21,7 @@ require('navbar.php');
               <div class="collapsible-body">
                   <span>
                     <div class="z-depth-5" style="background-color:white; padding-top:15px; padding-bottom:15px; height: 25rem;">
-                      <?php  imgText("","Img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
+                      <?php  imgText("","img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
                     </div>
                   </span>
               </div>
@@ -31,7 +31,7 @@ require('navbar.php');
               <div class="collapsible-body">
                   <span>
                      <div class="z-depth-5" style="background-color:white; padding-top:15px; padding-bottom:15px; height: 25rem;">
-                      <?php  imgText("","Img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
+                      <?php  imgText("","img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
                     </div>
                   </span>
               </div>
@@ -41,7 +41,7 @@ require('navbar.php');
               <div class="collapsible-body">
                   <span>
                       <div class="z-depth-5" style="background-color:white; padding-top:15px; padding-bottom:15px; height: 25rem;">
-                      <?php  imgText("","Img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
+                      <?php  imgText("","img/IMG_1602.jpeg", "m7", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",""); ?>
                     </div>
                   </span>
               </div>
