ea548aca02545444477e7fb03a3c5e5262b683f4
not nick <nicolaskeil@hotmail.com>
2017-05-18 13:44:49 -0700

    good things

--- LASAR-Underground-Construction/BID.php
+++ LASAR-Underground-Construction/BID.php
@@ -1,57 +1,40 @@
-<?php require('navbar.php');?>
-    <!--== BODY HTML CONTENT ==-->
-        <div class="container" style="padding: 70px;">
-         <form action="form.php" method="get">
-            <h1 style="font-size:50px;">Request a Bid</h1>
-           <div class="row">
-                <div class="input-field col s6">
-                <input type="text" id="Company" class="validate">
-                <label for="Company">Company</label>
-                </div>
-            </div>
-             <div class="row">
-                 <div class="input-field col s6">
-                  <input type="tel" id="phone">
-                  <label for="phone">Phone Number</label>
-                </div>
-             </div>
-              <div class="row">
-                 <div class="input-field col s6">
-                  <input type="email" id="email">
-                  <label for="email" data-error="Invalid Email">Email</label>
-              </div>
-             </div>
-              <div class="row">
-                 <div class="input-field col s6">
-                  <input type="text" id="Job">
-                  <label for="Job">Bid Title</label>
-              </div>
-             </div>
-             <div class="row">
-                 <div class="input-field col s6">
-                  <input type="text" id="BidLocation">
-                  <label for="BidLocation">Bid Location</label>
-              </div>
-             </div>
-              <div class="row">
-                   <div class="input-field col s6">
-                 <textarea id="jobDesc" class="materialize-textarea" data-length="500"></textarea>
-                    <label for="jobDesc">Bid Description</label>
-              </div>
-             </div>
-             <div class="row">
-                 <button class="btn waves-effect waves-light green darken-3" type="submit" name="action">Submit
-                    <i class="material-icons right">send</i>
-                  </button>
-             </div>
-         </form>
-         </div>
-<?php require('footer.php');?>
-   <script>$(document).ready(function(){
-      $('.parallax').parallax();
-    $('.button-collapse').sideNav({
-            menuWidth: 200, // Default is 300
-            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-            draggable: true // Choose whether you can drag to open on touch screens
-        });
-    });</script>
+<div class="container">
+ <form action="form.php" method="get">
+   <div class="row">
+        <div class="input-field col s12">
+        <input type="text" id="Company" class="validate">
+        <label for="Company">Company</label>
+        </div>
+    </div>
+     <div class="row">
+         <div class="input-field col s12">
+          <input type="tel" id="phone">
+          <label for="phone">Phone Number</label>
+        </div>
+     </div>
+      <div class="row">
+         <div class="input-field col s12">
+          <input type="email" id="email">
+          <label for="email" data-error="Invalid Email">Email</label>
+      </div>
+     </div>
+      <div class="row">
+         <div class="input-field col s12">
+          <input type="text" id="Job">
+          <label for="Job">Bid Title</label>
+      </div>
+     </div>
+     <div class="row">
+         <div class="input-field col s12">
+          <input type="text" id="BidLocation">
+          <label for="BidLocation">Bid Location</label>
+      </div>
+     </div>
+      <div class="row">
+           <div class="input-field col s12">
+         <textarea id="jobDesc" class="materialize-textarea" data-length="500"></textarea>
+            <label for="jobDesc">Bid Description</label>
+      </div>
+     </div>
+ </form>
+ </div>
